062af88a69a8b9a1df9922373df50c4797e532da
Artem <artem.a.baranov@yandex.ru>
2023-11-30 21:52:10 +0300

    initial commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var searchLayout = findViewById<LinearLayout>(R.id.search_button_layout);
+        var playlistLayout = findViewById<LinearLayout>(R.id.playlist_button_layout);
+        var settingsLayout = findViewById<LinearLayout>(R.id.settings_button_layout);
+        searchLayout.setOnClickListener {
+            val navigateToSearchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(navigateToSearchIntent)
+        }
+        val navigateToPlaylistIntent = Intent(this, MediatekaActivity::class.java)
+        val playlistOnclickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(navigateToPlaylistIntent)
+            }
+        }
+        playlistLayout.setOnClickListener(playlistOnclickListener)
+        settingsLayout.setOnClickListener {
+            val navigateToSettingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(navigateToSettingsIntent)
+        }
     }
 }
