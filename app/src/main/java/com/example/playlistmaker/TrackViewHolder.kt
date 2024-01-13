3cec9189a6a27d201875d34c986328e1b8fab88b
Artem <artem.a.baranov@yandex.ru>
2024-01-13 21:59:36 +0300

    ту громоздкую функцию заменил на getDimensionPixelSize, а то что название трека заканчивается на многоточие я починил в прошлый раз в track_item_view.xml - добавил wrap content для Linear layout, weight и ellipsize для textview

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -22,16 +22,9 @@ class TrackViewHolder(private val parent:ViewGroup,itemView: View = LayoutInflat
             .load(model.artworkUrl100)
             .placeholder(R.drawable.placeholder)
             .fitCenter()
-            .transform(RoundedCorners(dpToPx(itemView.context.resources.getDimension(R.dimen.track_image_rounding), itemView.context)))
+            .transform(RoundedCorners(itemView.context.resources.getDimensionPixelSize(R.dimen.track_image_rounding)))
             .into(trackImage)
         trackAuthor.text = model.artistName
         trackDuration.text = model.trackTime
-    }
-    //функция из методички, тут нельзя напрямую передавать значение в px поскольку оно зависит от размеров экрана
-    private fun dpToPx(dp: Float, context: Context): Int {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            dp,
-            context.resources.displayMetrics).toInt()
     }
 }
