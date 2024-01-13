68116ca83998861f838bfb772429b86893a55241
Artem <artem.a.baranov@yandex.ru>
2024-01-13 03:57:36 +0300

    fixes

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -22,11 +22,12 @@ class TrackViewHolder(private val parent:ViewGroup,itemView: View = LayoutInflat
             .load(model.artworkUrl100)
             .placeholder(R.drawable.placeholder)
             .fitCenter()
-            .transform(RoundedCorners(dpToPx(2f, itemView.context)))
+            .transform(RoundedCorners(dpToPx(itemView.context.resources.getDimension(R.dimen.track_image_rounding), itemView.context)))
             .into(trackImage)
         trackAuthor.text = model.artistName
         trackDuration.text = model.trackTime
     }
+    //функция из методички, тут нельзя напрямую передавать значение в px поскольку оно зависит от размеров экрана
     private fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
