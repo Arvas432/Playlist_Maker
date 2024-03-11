c7cde6afbe7d07c55ac675220e1d876d2a4743c1
Aras432 <artem.a.baranov@yandex.ru>
2024-03-11 17:57:19 +0300

    функционал аудио плеера, debounce экрана поиска, progress bar

--- app/src/main/java/com/example/playlistmaker/TrackListAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackListAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 
 class TrackListAdapter(private val tracks: List<Track>, private val searchHistory: SearchHistory) : RecyclerView.Adapter<TrackViewHolder>() {
+    private var isClickAllowed = true
+
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         return TrackViewHolder(parent)
     }
@@ -18,15 +24,25 @@ class TrackListAdapter(private val tracks: List<Track>, private val searchHistor
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
         holder.itemView.setOnClickListener{
-            searchHistory.write(tracks[position])
-            val navigateToPlayerActivity = Intent(holder.itemView.context, PlayerActivity::class.java)
-            Log.i("OPENING TRACK", "Opening track with the country: ${tracks[position].country}")
-            navigateToPlayerActivity.putExtra(TRACK_PLAYER_KEY,Gson().toJson(tracks[position]))
-            holder.itemView.context.startActivity(navigateToPlayerActivity)
+            if(clickDebounce()){
+                searchHistory.write(tracks[position])
+                val navigateToPlayerActivity = Intent(holder.itemView.context, PlayerActivity::class.java)
+                navigateToPlayerActivity.putExtra(TRACK_PLAYER_KEY,Gson().toJson(tracks[position]))
+                holder.itemView.context.startActivity(navigateToPlayerActivity)
+            }
         }
+    }
+    private fun clickDebounce() : Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
     }
     companion object{
         const val TRACK_PLAYER_KEY = "TRACK_PLAYER_KEY"
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
     }
 
 }
